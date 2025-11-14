e9dc5b504ec67a2a6535efccfd79df7538cee05a
Nazik-o <meimeilovessansan@gmail.com>
2025-11-13 19:57:17 -0800

    Added some cute details and a loading bar

--- src/main/java/com/bobashop/HomeScreen.java
+++ src/main/java/com/bobashop/HomeScreen.java
@@ -192,6 +192,7 @@ public class HomeScreen {
             }
         }
     }
+    //Starter
     public static void main(String[] args) {
         HomeScreen home = new HomeScreen();
         home.start();
